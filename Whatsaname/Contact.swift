965fe35f0d13a40539bbaf3bb973339fb9c8406f
Paul Richardson <developer@photographerafoot.com>
2020-10-11 12:03:12 +0200

    CHG: Add ImageSaver and Contact

---
+++ Whatsaname/Contact.swift
@@ -0,0 +1,19 @@
+//
+//  Contact.swift
+//  Whatsaname
+//
+//  Created by Paul Richardson on 11.10.2020.
+//
+
+import Foundation
+
+struct Contact: Identifiable, Comparable {
+	
+	let id = UUID()
+	let firstName: String
+	let lastName: String
+	
+	static func < (lhs: Contact, rhs: Contact) -> Bool {
+		lhs.lastName < rhs.lastName
+	}
+}
---
+++ Whatsaname/ImageSaver.swift
@@ -0,0 +1,21 @@
+//
+//  ImageSaver.swift
+//  Whatsaname
+//
+//  Created by Paul Richardson on 11.10.2020.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+	
+	var successHandler: (() -> Void)?
+	var errorHandler: ((Error) -> Void)?
+	
+	func writetoJSON(image: UIImage) {
+		
+		
+		
+	}
+	
+}
